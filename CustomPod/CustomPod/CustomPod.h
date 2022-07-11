3449547f31d8eacc3ac438a20e79bca7f30c48ed
shabnam4potenza <81210629+shabnam4potenza@users.noreply.github.com>
2022-07-11 13:22:09 +0530

    inital commit

---
+++ CustomPod/CustomPod/CustomPod.h
@@ -0,0 +1,18 @@
+//
+//  CustomPod.h
+//  CustomPod
+//
+//  Created by Apple on 11/07/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CustomPod.
+FOUNDATION_EXPORT double CustomPodVersionNumber;
+
+//! Project version string for CustomPod.
+FOUNDATION_EXPORT const unsigned char CustomPodVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CustomPod/PublicHeader.h>
+
+
---
+++ CustomPod/CustomPod/CustomPod.swift
@@ -0,0 +1,21 @@
+//
+//  CustomPod.swift
+//  CustomPod
+//
+//  Created by Apple on 11/07/22.
+//
+
+import Foundation
+public final class CustomPod {
+
+    let name = "CustomPod"
+    
+    public func add(a: Int, b: Int) -> Int {
+        return a + b
+    }
+    
+    public func sub(a: Int, b: Int) -> Int {
+        return a - b
+    }
+    
+}
